0ffc2be553e6b9d54843e5e95a5ca04a59cc9043
tuanpm <tuanpm@novaon.vn>
2018-04-21 13:38:30 +0700

    up
    Up

---
+++ DataAccess/DAL/Exampale.cs
@@ -0,0 +1,45 @@
+﻿using Dapper;
+using DBO.DATA;
+using ERP.Framework.Common;
+using ERP.Framework.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.DAL
+{
+    public class Exampale : BaseDal<ADOProvider>
+    {
+
+        //public IList<Contract> GetContracts(BaseListParam listParam, out int? totalRecord)
+        //{
+        //    try
+        //    {
+        //        var param = new DynamicParameters();
+        //        param.Add("@FilterField", listParam.FilterField);
+        //        param.Add("@OrderByField", listParam.OrderByField);
+        //        param.Add("@PageIndex", listParam.PageIndex);
+        //        param.Add("@PageSize", listParam.PageSize);
+        //        param.Add("@UserId", listParam.UserId);
+        //        param.Add("@RoleId", listParam.UserType);
+        //        param.Add("@DeptId", listParam.DeptId);
+        //        param.Add("@LanguageID", listParam.LanguageCode);
+        //        param.Add("@TotalRecord", dbType: DbType.Int32, direction: ParameterDirection.Output);
+        //        var values = UnitOfWork.Procedure<Contract>("Contract_Gets", param).ToList();
+        //        totalRecord = param.GetDataOutput<int>("@TotalRecord");
+        //        return values;
+        //    }
+        //    catch (Exception)
+        //    {
+        //        totalRecord = 0;
+        //        return new List<Contract>();
+        //    }
+        //}
+        public IList<Exam> GetContracts()
+        {
+            return UnitOfWork.Procedure<Exam>("Exam_GetAll").ToList();
+        }
+    }
+}
---
+++ TMDT/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using DataAccess.DAL;
+using DataAccess.DATA;
+using ERP.Framework.Singleton;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TMDT.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            Exampale exam = new Exampale();
+            var list = exam.GetContracts();
+            var list1 = SingletonIpl.GetInstance<Exampale>().GetContracts();
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "abc";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
